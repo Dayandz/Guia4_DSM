a3769ea07980bcf6d4610289932449d0faade67b
Dayanna <dzuniga145@gmail.com>
2022-03-24 00:22:28 -0600

    guia 4

---
+++ Guia07App/app/src/main/java/sv/edu/udb/dsm/guia3/guia07app/DashboardActivity.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.dsm.guia3.guia07app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
